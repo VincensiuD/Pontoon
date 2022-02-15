98d1f59d8bd6e93a35cc6867117415bf8d20fab1
VincensiuD <spanishfrenchromanian@gmail.com>
2022-02-15 18:05:09 +1100

    Commit changes to backend, creating new DTO

---
+++ Pontoon/Models/PontoonDTO.cs
@@ -0,0 +1,26 @@
+﻿using Pontoon.Data;
+
+namespace Pontoon.Models
+{
+    public class PontoonDTO
+    {
+
+
+
+        public int Money { get; set; }
+        public List<string> DealerCardsDisplayCodes { get; set; }
+        public List<string> PlayerCardsDisplayCodes { get; set; }
+        public int PairsBetResult { get; set; }
+        public int MainBetResult { get; set; }
+        public int DealerTotal { get; set; }
+        public int PlayerTotal { get; set; }
+        public string PlayerTotal2 { get; set; }
+        public string DealerTotal2 {get; set;}
+        public bool HitBtn { get; set; }
+        public bool SplitBtn { get; set; }
+        public bool DoubleBtn { get; set; }
+        public string DisplayMessage { get; set; }
+        public List<string> PlayerCardsADisplayCodes  { get; set; }
+        public List<string> PlayerCardsBDisplayCodes { get; set; }
+    }
+}
