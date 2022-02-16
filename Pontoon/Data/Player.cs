b3278f20a80347a726fab3ea7d80c19a59ecacda
VincensiuD <spanishfrenchromanian@gmail.com>
2022-02-17 00:06:45 +1100

    commit

---
+++ Pontoon/Data/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace Pontoon.Data
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get ; set; }
+        public Wallet Wallet { get; set; }
+        public Wager Wager { get; set; }
+        public CardSequence CardSequence { get; set; }
+       // public List<Card> EigthDeckCards { get; set; }
+       public Card[] CardDecks
+        {
+            get;
+            set;
+        }
+    }
+}
